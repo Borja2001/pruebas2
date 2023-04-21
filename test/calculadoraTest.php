fd93ac95d58a567fbd466e31d654e73962d0adb6
Borja2001 <bopres@floridauniversitaria.es>
2023-04-21 16:18:16 +0200

    estoy arto

---
+++ test/calculadoraTest.php
@@ -0,0 +1,22 @@
+<<?php
+use PHPUnit \Framework\TestCase;
+include 'Calculadora.php';
+final class CalculadoraTest extends TestCase
+{
+public function testSuma()
+{
+$calc = new Calculadora(3,4);
+$this->assertEquals(8, $calc->suma()); 
+}
+public function testResta()
+{
+$calc = new Calculadora(3,4);
+$this->assertEquals(1, $calc->resta()); 
+}
+public function testMultiplicacion()
+{
+$calc = new Calculadora(3,4);
+$this->assertEquals(12, $calc->multiplicacion()); 
+}
+}
+?>
